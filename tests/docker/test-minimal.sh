e5e862be0d831192fef343e58efe4308b08accb2
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-22 20:46:04 +0200

    Remove params

--- tests/docker/test-minimal.sh
+++ tests/docker/test-minimal.sh
@@ -10,7 +10,7 @@ curl -s https://raw.githubusercontent.com/phnmnl/wft4galaxy/develop/utils/docker
 cd ${IMAGE_ROOT_PATH}
 
 # build docker image
-./build-remote.sh --url="https://github.com/${TRAVIS_REPO_SLUG}" --branch=${TRAVIS_BRANCH} && cd -
+./build-remote.sh && cd -
 
 # Run examples  # FIXME: change the repo and version TAGs
 wft4galaxy-docker --repository kikkomep --version minimal-travis-integration \
