bcbf702e171f605ee592a4aabc63f834308dbfd0
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-23 08:51:08 +0200

    Support custom Docker network

--- utils/docker/start-galaxy.sh
+++ utils/docker/start-galaxy.sh
@@ -2,13 +2,15 @@
 
 # set args
 export GALAXY_CONFIG_MASTER_API_KEY=${1:-"HSNiugRFvgT574F43jZ7N9F3"}
-export GALAXY_PORT=${2:-30700}
-export GALAXY_DOCKER_HOST=${3:-"localhost"}
-export GALAXY_CONTAINER_NAME=${4:-"galaxy-server"}
+export GALAXY_NETWORK=${2:-"galaxy"}
+export GALAXY_PORT=${3:-30700}
+export GALAXY_DOCKER_HOST=${4:-"localhost"}
+export GALAXY_CONTAINER_NAME=${5:-"galaxy-server"}
 
 # start Dockerized Galaxy
 docker run -d -e GALAXY_CONFIG_MASTER_API_KEY=${GALAXY_CONFIG_MASTER_API_KEY=HSNiugRFvgT574F43jZ7N9F3} \
               -p ${GALAXY_PORT}:80 -p 8021:21 -p 8022:22 \
+              --network ${GALAXY_NETWORK} \
               --name ${GALAXY_CONTAINER_NAME} \
               bgruening/galaxy-stable
 
