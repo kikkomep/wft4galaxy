40910e04d3b6dda6bc138b1576a81ef8752b99ba
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-23 16:11:46 +0200

    Update Docker settings

--- tests/docker/test-minimal.sh
+++ tests/docker/test-minimal.sh
@@ -5,6 +5,7 @@ IMAGE_ROOT_PATH=${1:-"utils/docker/minimal"}
 # TODO: implement a better arg parsing
 GALAXY_URL=$2
 GALAXY_API_KEY=$3
+GALAXY_NETWORK=$4
 
 # download wft4galaxy script
 curl -s https://raw.githubusercontent.com/phnmnl/wft4galaxy/develop/utils/docker/install.sh | bash /dev/stdin .
@@ -15,9 +16,13 @@ cd ${IMAGE_ROOT_PATH}
 # build docker image
 ./build-remote.sh --branch ${TRAVIS_BRANCH} && cd -
 
+# FIXME: remove... it is just for debug
+docker --rm --network galaxy \
+            ubuntu bash -c "apt-get update && apt-get install -y iputils-ping && timeout 10 ping 172.18.0.22"
+
+
 # Run examples  # FIXME: change the repo and version TAGs
 wft4galaxy-docker --skip-update --repository kikkomep --version minimal-travis-integration \
-                  --network "galaxy" \
-                  --add-host "galaxy-server:172.18.0.22" \
+                  --network ${GALAXY_NETWORK} --debug \
                   --server ${GALAXY_URL} --api-key ${GALAXY_API_KEY} \
                   -f examples/change_case/workflow-test.yml
--- utils/docker/start-galaxy.sh
+++ utils/docker/start-galaxy.sh
@@ -3,16 +3,17 @@
 # set args
 export GALAXY_CONFIG_MASTER_API_KEY=${1:-"HSNiugRFvgT574F43jZ7N9F3"}
 export GALAXY_NETWORK=${2:-"galaxy"}
-export GALAXY_PORT=${3:-30700}
-export GALAXY_DOCKER_HOST=${4:-"localhost"}
-export GALAXY_CONTAINER_NAME=${5:-"galaxy-server"}
+export GALAXY_ADDRESS=${3:-127.0.0.1}
+export GALAXY_PORT=${4:-30700}
+export GALAXY_DOCKER_HOST=${5:-"localhost"}
+export GALAXY_CONTAINER_NAME=${6:-"galaxy-server"}
 
 # start Dockerized Galaxy
 docker run -d -e GALAXY_CONFIG_MASTER_API_KEY=${GALAXY_CONFIG_MASTER_API_KEY=HSNiugRFvgT574F43jZ7N9F3} \
-              -p galaxy:${GALAXY_PORT}:80 -p 8021:21 -p 8022:22 \
+              -p ${GALAXY_PORT}:80 -p 8021:21 -p 8022:22 \
               --network ${GALAXY_NETWORK} \
               --name ${GALAXY_CONTAINER_NAME} \
-              --ip 172.18.0.22 \
+              --ip ${GALAXY_ADDRESS} \
               bgruening/galaxy-stable
 
 # set the Galaxy URL
