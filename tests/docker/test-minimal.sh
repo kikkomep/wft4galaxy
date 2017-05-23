2d65ce8c7e4a5ff41bdacdc09f87fcea09a9e541
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-23 09:51:21 +0200

    Fix test-minimal URL and API-KEY

--- tests/docker/test-minimal.sh
+++ tests/docker/test-minimal.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+GALAXY_URL=$1
+GALAXY_API_KEY=$2
+
 # FIXME: check path
 IMAGE_ROOT_PATH=${1:-"utils/docker/minimal"}
 
@@ -15,5 +18,5 @@ cd ${IMAGE_ROOT_PATH}
 # Run examples  # FIXME: change the repo and version TAGs
 wft4galaxy-docker --skip-update --repository kikkomep --version minimal-travis-integration \
                   --network "galaxy" \
-                  --server ${GALAXY_URL} --api-key ${GALAXY_CONFIG_MASTER_API_KEY} \
+                  --server ${GALAXY_URL} --api-key ${GALAXY_API_KEY} \
                   -f examples/change_case/workflow-test.yml
